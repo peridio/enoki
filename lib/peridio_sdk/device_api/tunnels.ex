4ec2fa6703b623a5c5d6a03d15144423b2576444
Justin Schneck <j.schneck@peridio.com>
2024-05-16 13:16:41 -0400

    device api: Add list / close to tunnels

--- lib/peridio_sdk/device_api/tunnels.ex
+++ lib/peridio_sdk/device_api/tunnels.ex
@@ -2,11 +2,29 @@ defmodule PeridioSDK.DeviceAPI.Tunnels do
   alias PeridioSDK.HTTPClient
   alias PeridioSDK.DeviceAPI
 
+  def list(client, query_params \\ []) do
+    HTTPClient.get(
+      client,
+      DeviceAPI.middleware(client),
+      "/tunnels",
+      query: query_params
+    )
+  end
+
   def configure(client, tunnel_prn, %{cidr_blocks: _, port_ranges: _, device_proxy_port: _, device_tunnel_port: _, device_public_key: _} = tunnel_data) do
     HTTPClient.post(
       client,
       DeviceAPI.middleware(client),
       "/tunnels/#{tunnel_prn}/configure",
+      tunnel_data
+    )
+  end
+
+  def update(client, tunnel_prn, %{state: _} = tunnel_data) do
+    HTTPClient.patch(
+      client,
+      DeviceAPI.middleware(client),
+      "/tunnels/#{tunnel_prn}/update",
       tunnel_data
     )
   end
